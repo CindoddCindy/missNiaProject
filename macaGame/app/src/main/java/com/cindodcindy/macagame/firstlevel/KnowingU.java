ec11a2dd6e70efd8cc6dd39f1fb74ea475df628d
CindoddCindy <cindodcindy@gmail.com>
2021-06-01 13:12:51 +0700

    tambah asset

---
+++ macaGame/app/src/main/java/com/cindodcindy/macagame/firstlevel/KnowingU.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.macagame.firstlevel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.macagame.R;
+
+public class KnowingU extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knowing_u);
+    }
+}
